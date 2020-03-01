4b3ec5851bb771d96224cb43497c4411d89fa728
Tomas <tomas.huzevka@outlook.com>
2020-02-29 20:06:28 -0500

    Dojos_and_ninjas

---
+++ config.py
@@ -0,0 +1,12 @@
+from flask import Flask			
+from flask_sqlalchemy import SQLAlchemy			                   
+from flask_migrate import Migrate		
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db_for_ninja.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
