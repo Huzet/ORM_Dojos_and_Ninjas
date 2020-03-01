3870203d010466c317e9bc61013b01e9d3e23e4d
Tomas <tomas.huzevka@outlook.com>
2020-02-29 20:26:08 -0500

    added models

--- app.py
+++ app.py
@@ -1,24 +1,6 @@
-from flask import Flask, render_template, request, redirect					
-from sqlalchemy.sql import func                         
+from flask import Flask, render_template, request, redirect
 from config import app, db, migrate
-
-class ninjas(db.Model):	
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(45))
-    last_name = db.Column(db.String(45))
-    dojo_name = db.Column(db.String(45))
-    dojos_id = db.Column(db.Integer, db.ForeignKey("dojos.id"))
-    dojo = db.relationship("dojos", foreign_keys=[dojos_id], backref="ninjas", cascade="all")
-    created_at = db.Column(db.DateTime, server_default=func.now())  
-    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-
-class dojos(db.Model):	
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(45))
-    city = db.Column(db.String(45))
-    state = db.Column(db.String(45))
-    created_at = db.Column(db.DateTime, server_default=func.now())  
-    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+from models import ninjas, dojos
 
 @app.route("/")
 def index():
